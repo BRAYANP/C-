a2a70167b189177a34450582e8c1800416ad6ccf
ADMOBILIZE MAC MINI <ADMOBILIZE@ADMOBILIZEs-Mac-mini.local>
2016-06-17 14:06:57 -0500

    fake commit

---
+++ TriangleZone.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math>
+
+using  namespace std ;
+
+int main (){
+
+        float base, height , area  ;
+
+        area = base * height / 2;
+        cout<< " Enter The Base" ;
+        cin>> base;
+        cout<<"Enter The Height";
+        cin>>height;
+        cout<<"The triangle Zone is : "<<area <<endl;
+
+
+}
---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <math>
+
+
+
+using namespace std;
+
+/*
+using namespace std;
+
+int main() {
+    cout << "Hello, World! \n" << "Hello, World!" << endl;
+    cout << "Hello, World!" << endl;
+    // validation of average from NOTES
+
+    double   note1 , note2, note3,note4,finalNote;
+
+
+    cout << " ENTER NOTE 1 : "<< endl;
+    cin >> note1 ;
+    cout << " ENTER NOTE 2 : "<< endl;
+    cin >> note2 ;
+    cout << " ENTER NOTE 3 : "<< endl;
+    cin >> note3 ;
+    cout << " ENTER NOTE 4 : "<< endl;
+    cin >> note4 ;
+
+
+    finalNote = (note1 + note2 + note3 + note4) / 4 ;
+    cout << " Your Note Final Is : " <<finalNote <<endl;
+
+}*/
+int main() {
+
+}
