7a86f842fb1e1fc0a43efb2f14e31051b45f6a04
ADMOBILIZE MAC MINI <ADMOBILIZE@ADMOBILIZEs-Mac-mini.local>
2016-06-17 14:21:22 -0500

    fix

--- main.cpp
+++ main.cpp
@@ -1,35 +1,30 @@
 #include <iostream>
-#include <math>
+#include <cmath>
 
 
 
 using namespace std;
 
-/*
-using namespace std;
 
-int main() {
-    cout << "Hello, World! \n" << "Hello, World!" << endl;
-    cout << "Hello, World!" << endl;
-    // validation of average from NOTES
-
-    double   note1 , note2, note3,note4,finalNote;
-
-
-    cout << " ENTER NOTE 1 : "<< endl;
-    cin >> note1 ;
-    cout << " ENTER NOTE 2 : "<< endl;
-    cin >> note2 ;
-    cout << " ENTER NOTE 3 : "<< endl;
-    cin >> note3 ;
-    cout << " ENTER NOTE 4 : "<< endl;
-    cin >> note4 ;
-
-
-    finalNote = (note1 + note2 + note3 + note4) / 4 ;
-    cout << " Your Note Final Is : " <<finalNote <<endl;
-
-}*/
-int main() {
-
+int sumar(int x ,  int  y){
+    return  x + y;
 }
+
+int main() {
+    float base, height , area  ;
+
+    area = base * height / 2;
+    cout<< " Enter The Base" ;
+    cin>> base;
+    cout<<"Enter The Height";
+    cin>>height;
+    cout << "The triangle Zone is : "<<area << endl;
+    cout << "The sum(3,4) : "<< sumar( 3,4) << endl;
+
+
+
+    return 0;
+}
+
+
+
